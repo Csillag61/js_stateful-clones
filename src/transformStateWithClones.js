9a0ae82a2590f492f5ca5b911decaa2f30c452b0
Csillag61 <csillagok@gmail.com>
2025-03-25 11:36:14 -0400

    updated solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -15,31 +15,39 @@ function transformStateWithClones(state, actions) {
       throw new Error(`Invalid action object: ${JSON.stringify(action)}`);
     }
 
-    if (action.type === 'clear') {
-      currentState = {};
-    } else if (action.type === 'addProperties') {
-      if (!action.extraData || typeof action.extraData !== 'object') {
-        throw new Error(
-          `Invalid extraData: ${JSON.stringify(action.extraData)}`,
-        );
-      }
-      currentState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      if (!Array.isArray(action.keysToRemove)) {
-        throw new Error(
-          `Invalid keysToRemove: ${JSON.stringify(action.keysToRemove)}`,
-        );
-      }
-      currentState = { ...currentState };
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
 
-      action.keysToRemove.forEach((key) => {
-        delete currentState[key];
-      });
-    } else {
-      throw new Error(`Unknown action type: ${action.type}`);
+      case 'addProperties':
+        if (!action.extraData || typeof action.extraData !== 'object') {
+          throw new Error(
+            `Invalid extraData: ${JSON.stringify(action.extraData)}`,
+          );
+        }
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        if (!Array.isArray(action.keysToRemove)) {
+          throw new Error(
+            `Invalid keysToRemove: ${JSON.stringify(action.keysToRemove)}`,
+          );
+        }
+        currentState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    stateHistory.push(currentState);
+    // Push a copy of the current state to the history array
+    stateHistory.push({ ...currentState });
   });
 
   return stateHistory;
